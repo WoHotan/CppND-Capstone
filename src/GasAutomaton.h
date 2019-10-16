9359e980ca6b466d41bd787d9e5bc7d34699b18e
温勋庆 <wenxunqing@wenxunqingde-Mac-mini.local>
2019-10-16 23:41:39 +0800

    GasAutomaton

--- src/GasAutomaton.h
+++ src/GasAutomaton.h
@@ -7,22 +7,52 @@
 
 namespace Automata {
     using dataType = unsigned short;
-    class GasAutomaton : public Cellular<dataType, 400>
+    class GasAutomaton : public CellularAutomaton<dataType, 400>
     {
     private:
-        /* data */
+        enum Direction : unsigned {
+          Northwest  = 0, North, Northeast, East, Southeast, South, Southwest, West, Solid
+      };
+        cv::Mat frame;
+        cv::VideoWriter writer;
+        std::string outputPath;
+        void init() noexcept override;
+        void applyRule() override;
+        void draw() override;
+        template<unsigned n>
+        inline constexpr void set(unsigned x, unsigned y, bool value = true){
+            static_assert(n < bitsInDataType, "Invalid offset");
+            if(value){
+              _nextLattice(x, y) |= (1U << n);
+            }
+            else{
+                _nextLattice(x, y) &= ~(1U << n);
+            }
+            
+        }
+
+        template<unsigned n>
+        inline constexpr bool get(unsigned x, unsigned y){
+            static_assert(n < bitsInDataType, "Invalid offset for byte");
+            if(value){
+                _nextLattice(x, y) |= (1U << n);
+            }
+            else{
+              _nextLattice(x, y) &= ~(1U << n);
+            }
+            
+        }
+
+        template<unsigned n>
+        inline constexpr bool get(unsigned x, unsigned y) {
+            static_assert(n < bitsInDataType, "Invalid offset for byte");
+            return (_currentLattice(x, y) & (1U << n)) > 0;
+        }
+
     public:
-        GasAutomaton : public Cellular(/* args */);
-        ~GasAutomaton : public Cellular();
+        static constexpr auto bitsInDataType = sizeof(dataType) << 3UL;
+        GasAutomaton(size_t totalSteps, std::string outputPath);
+        virtual ~GasAutomaton() noexcept;
     };
-    
-    GasAutomaton : public Cellular::GasAutomaton(/* args */)
-    {
-    }
-    
-    GasAutomaton : public Cellular::~GasAutomaton()
-    {
-    }
-    
 }
 #endif
