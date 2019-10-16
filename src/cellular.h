5af7f4c6a8064c63e5716cd22ef9eb2c5e7763da
温勋庆 <wenxunqing@wenxunqingde-Mac-mini.local>
2019-10-16 22:29:55 +0800

    lattice

---
+++ src/cellular.h
@@ -0,0 +1,6 @@
+#ifdef CELLULAR_H
+#define CELLULAR_H
+
+#include <random>
+#include "lattice.h"
+#endif
---
+++ src/lattice.h
@@ -0,0 +1,39 @@
+#ifndef LATTICE_H
+#define LATTICE_H
+
+#include <cstddef>
+#include <algorithm>
+
+namespace Automata{
+    template<class T, std::size_t _width, std::size_t _height = _width, std::size_t _align = 0>
+    struct Lattice
+    {
+      public:
+          static constexpr auto height = _height;
+          static constexpr auto width = _width;
+
+          T& operator()(std::size_t x, std::size_t y){
+              auto index = (y * width) + x;
+              if ((x >= width) || (y >= height)){
+                  throw std::out_of_range("out of range");
+              }
+              return data[index];
+          }
+
+          T operator()(std::size_t x, std::size_t y) const{
+              auto index = (y * width) + x;
+              if((x >= width) || (y >= height)){
+                  throw std::out_of_range("out of range");
+              }
+              return data[index];
+          }
+
+          void swap(Lattice& other){
+              std::swap(data, other.data);
+          }
+      private:
+          alignas(_align) T data[height * width];
+    };
+    
+}
+#endif
