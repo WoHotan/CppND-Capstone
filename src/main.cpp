bcafe75d29e8b864dbd575f3d8b3fc71b8a56bd8
温勋庆 <wenxunqing@wenxunqingde-Mac-mini.local>
2019-10-17 20:33:13 +0800

    start action

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "GasAutomaton.h"
 
-int main() {
-    std::cout << "Hello World!" << "\n";
+int main(int argc, char* argv[]) {
+    unsigned iterations = 3000;
+    if(argc > 1){
+        iterations = std::stoi(argv[1]);
+    }
+    std::cout << "Starting GasAutomaton!" << std::endl;
+    Automato::GasAutomaton gasAutomaton(iterations, "./gasAutomaton.avi");
+    gasAutomaton.execute();
     return 0;
 }
